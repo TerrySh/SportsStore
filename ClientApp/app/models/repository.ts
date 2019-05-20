d63662a9d0263f09351b6f0ebe383024d54d02e7
Terry Shi <tianliangshi@outlook.com>
2019-05-20 18:44:40 +0800

    Chapter6-implementedWholeWebServices

--- ClientApp/app/models/repository.ts
+++ ClientApp/app/models/repository.ts
@@ -4,8 +4,10 @@ import { Http, RequestMethod, Request, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Filter } from './configClasses.repository';
+import { Supplier } from './supplier.model';
 
 const productsUrl = "/api/products";
+const suppliersUrl = "/api/suppliers"
 
 @Injectable()
 export class Repository {
@@ -14,11 +16,18 @@ export class Repository {
 
   private productData: Product;
   products: Product[];
+  suppliers: Supplier[] = [];
 
   constructor(private http: Http) {
-    this.filter.category = "soccer";
+    //this.filter.category = "soccer";
     this.filter.related = true;
     this.getProducts();
+  }
+
+  private sendRequest(verb: RequestMethod, url: string, data?: any){
+    return this.http.request(new Request({
+      method: verb, url: url, body: data
+    })).map(response => response.headers.get("Content-Length") != '0' ? response.json() : null);
   }
 
   getProduct(id: number) {
@@ -43,12 +52,6 @@ export class Repository {
       .subscribe(response => this.products = response);
   }
 
-  private sendRequest(verb: RequestMethod, url: string, data?: any){
-    return this.http.request(new Request({
-      method: verb, url: url, body: data
-    })).map(response => response.json());
-  }
-
   get product(): Product {
     console.log('Product Data Requested');
     return this.productData;
@@ -57,4 +60,83 @@ export class Repository {
   get filter(): Filter {
     return this.filterObject;
   }
+
+  getSuppliers() {
+    this.sendRequest(RequestMethod.Get, suppliersUrl)
+    .subscribe(response => this.suppliers = response);
+  }
+
+  createProduct(prod: Product) {
+    let data = {
+      name: prod.name, category: prod.category,
+      description: prod.description, price: prod.price,
+      supplier: prod.supplier ? prod.supplier.supplierId : 0
+    };
+    this.sendRequest(RequestMethod.Post, productsUrl, data)
+    .subscribe(response => {
+      prod.productId = response;
+      this.products.push(prod);
+    });
+  }
+
+  createProductAndSupplier(prod: Product, supp: Supplier) {
+    let data = {
+      name: supp.name, city: supp.city, state: supp.state
+    };
+    this.sendRequest(RequestMethod.Post, suppliersUrl, data)
+      .subscribe(response => {
+        supp.supplierId = response;
+        prod.supplier = supp;
+        this.suppliers.push(supp);
+        if (prod != null) {
+          this.createProduct(prod);
+      }
+    });
+  }
+
+  replaceProduct(prod: Product) {
+    let data = {
+      name: prod.name, category: prod.category,
+      description: prod.description, price: prod.price,
+      supplier: prod.supplier ? prod.supplier.supplierId : 0
+    };
+    this.sendRequest(RequestMethod.Put, productsUrl + "/" + prod.productId, data)
+    .subscribe(response => this.getProducts());
+  }
+
+  replaceSupplier(supp: Supplier) {
+    let data = {
+      name: supp.name, city: supp.city, state: supp.state
+    };
+    this.sendRequest(RequestMethod.Put, suppliersUrl + "/" + supp.supplierId, data)
+      .subscribe(response => this.getProducts());
+  }
+
+  updateProduct(id: number, changes: Map<string, any>) {
+    let patch = [];
+    changes.forEach((value, key) =>
+      patch.push({ op: "replace", path: key, value: value }));
+    this.sendRequest(RequestMethod.Patch, productsUrl + "/" + id, patch)
+    .subscribe(response => this.getProducts());
+  }
+
+
+  deleteProduct(id: number) {
+    this.sendRequest(RequestMethod.Delete, productsUrl + "/" + id)
+    .subscribe(response => this.getProducts());
+  }
+
+  deleteSupplier(id: number) {
+    this.sendRequest(RequestMethod.Delete, suppliersUrl + "/" + id)
+    .subscribe(response => {
+      this.getProducts();
+      this.getSuppliers();
+    });
+  }
+
+
+
+
+
+
 }
